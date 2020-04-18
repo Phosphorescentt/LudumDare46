3a8ec27d2c1f1a962343766ca3fa2ac13432c0df
Josh Stafford <jegstafford@gmail.com>
2020-04-18 10:27:14 +0100

    Added ranged boolean

--- LD46/Assets/Scripts/EnemyController.cs
+++ LD46/Assets/Scripts/EnemyController.cs
@@ -8,6 +8,7 @@ public class EnemyController : MonoBehaviour
     GameObject player;
     public float range = 5;
     public float speed = 0.7f;
+    public bool ranged = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,7 @@ public class EnemyController : MonoBehaviour
 
         Vector3 dist = this.transform.position - player.transform.position;
 
-        if (dist.magnitude < range)
+        if (dist.magnitude < range || !ranged)
         {
             Debug.DrawLine(this.transform.position, player.transform.position);
             this.transform.position = this.transform.position - (Vector3.Normalize(dist) * speed) * Time.deltaTime;
