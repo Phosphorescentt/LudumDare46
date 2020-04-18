fc840ff6d6c0bb0adad0d1957af574f962f54aff
Joshua Mankelow <joshua@mankelow.org>
2020-04-18 16:05:13 +0100

    Changed a couple of settings

--- LD46/Assets/Scripts/RoomDestroyer.cs
+++ LD46/Assets/Scripts/RoomDestroyer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RoomDestroyer : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other) {
-        Destroy(other.gameObject);
+        if (other.CompareTag("Spawn Point")) {
+            Destroy(other.gameObject);
+        }
     }
 }
